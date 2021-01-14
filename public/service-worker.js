ed731aa113d9eb8be3faab2947518c5229742f2c
Chris Lyko <chrislyko@gmail.com>
2021-01-14 18:05:52 -0500

    added installer in sw

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,4 +6,17 @@ const FILES_TO_CACHE = [
     '/indexedDb.js',
     'https://fonts.googleapis.com/css?family=Istok+Web|Montserrat:800&display=swap',
     'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css',
-  ];
+];
+
+  
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open(PRECACHE)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
+      .then(self.skipWaiting())
+  );
+});
